ec0f2ca44de17882a572dead23cb83bc98e3c29b
Techie-123 <96535261+Techie-123@users.noreply.github.com>
2022-03-09 17:11:01 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+player1_score = 0;
+player2_score = 0;
+
+var player1_name = document.getElementById("player1_login").value;
+var player2_name = document.getElementById("player2_login").value;
+
+var question_turn = document.getElementById("Question Turn : " + player1_name);
+var answer_turn = document.getElementById("Answer Turn : " + player2_name);
